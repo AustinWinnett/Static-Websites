7ae13e4d310e6657fd0c881a85dfb295fcdb4f04
Austin <awinnett1@hotmail.com>
2016-06-16 15:23:11 -0500

    Added AJAX

--- DaniReserPhotography/assets/js/custom.js
+++ DaniReserPhotography/assets/js/custom.js
@@ -1,3 +1,44 @@
+var $loading = $('#loadingDiv'), timer;
+
+$loading.hide()
+$(document)
+    .ajaxStart(function () {
+        timer && clearTimeout(timer);
+        timer = setTimeout(function() {
+            $loading.show();
+        }, 1000);
+    })
+    .ajaxStop(function () {
+        clearTimeout(timer);
+        $loading.hide();
+    });
+
+/* ---- */
+
+$(document).ready(function() {
+    
+    $("body").fadeIn(1000);
+    
+});
+
+$.get("home.html", function(data){
+    $("#content").html(data).fadeIn(500);
+});
+
+function menuChoice(choice) {
+    
+    var page = choice;
+    $("li").removeClass("active");
+    $("#content, .activePage").fadeOut(500);
+    
+    setTimeout(function() {
+        $.get(page + ".html", function(data){
+            $("#content").html(data).fadeIn(500);
+        });
+    }, 500);
+    
+};
+
 $(".menuTrigger").click(function() {
     
     if ($(".naviMenu").is(":visible")) {
@@ -18,6 +59,49 @@ $(".menuTrigger").click(function() {
         
     };
     
+});
+
+$(".naviMenu").find("li").click(function() {
     
+    $(".naviMenu").slideUp(1000);
+    $(".fa").css({'-webkit-transform' : 'rotate('+ 360 +'deg)',
+                 '-moz-transform' : 'rotate('+ 360 +'deg)',
+                 '-ms-transform' : 'rotate('+ 360 +'deg)',
+                 'transform' : 'rotate('+ 360 +'deg)'});
+    
+})
+
+$(".home").click(function() {
+    
+    menuChoice("Home");
+    $(".home").addClass("active");
+    
+});
+
+$(".family").click(function() {
+    
+    menuChoice("Family");
+    $(".family").addClass("active");
+    
+});
+
+$(".wedding").click(function() {
+    
+    menuChoice("wedding");
+    $(".wedding").addClass("active");
+    
+});
+
+$(".event").click(function() {
+    
+    menuChoice("event");
+    $(".event").addClass("active");
+    
+});
+
+$(".contact").click(function() {
+    
+    menuChoice("contact");
+    $(".contact").addClass("active");
     
 });
